944f53be4eaf2c8f699a0f8fedbae5fab8214dbe
名護睦裕 <nagomutsuhiro@nagobokuhiroshinoMacBook.local>
2026-04-29 08:53:15 +0900

    CORS設定追加

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.json());
 app.use(express.static('public'));
 
